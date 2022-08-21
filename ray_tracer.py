4bd5d305c90f92e5b27ea74662ab4612fb8c3b98
schulze-paul <schulze.paul@protonmail.com>
2022-08-21 11:58:01 +0200

    Implementing ray_color class

--- ray_tracer.py
+++ ray_tracer.py
@@ -2,11 +2,12 @@ from dataclasses import dataclass
 from tqdm import tqdm
 import numpy as np
 
+
 def write_color(out_file, pixel_color: np.ndarray) -> None:
     red = int(255.999*pixel_color[0])
     green = int(255.999*pixel_color[1])
     blue = int(255.999*pixel_color[2])
-    
+
     rgb_string = f" {red} {green} {blue}\n"
     out_file.write(rgb_string)
 
@@ -17,40 +18,53 @@ class Ray():
     origin: np.ndarray
     direction: np.ndarray
 
-    def origin(self) -> np.ndarray:
-        """get origin of ray"""
-        return self.origin
+    def at(self, time: float) -> np.ndarray:
+        """get position of ray at time"""
+        return self.origin + time*self.direction
 
-    def direction(self) -> np.ndarray:
-        """get direction of ray"""
-        return self.direction
 
-    def at(self, t: float) -> np.ndarray:
-        """get position of ray at t"""
-        return self.origin + t*self.direction
+def ray_color(ray: Ray) -> np.ndarray:
+    """computes rgb color of a ray"""
+    unit_direction = ray.direction / np.linalg.norm(ray.direction)
+    time = 0.5*unit_direction[1] + 0.5
+    return (1 - time) * np.array([1.0, 1.0, 1.0]) + time*np.array([0.5, 0.7, 1.0])
+
 
 def main():
     """main function of the ray tracer"""
     # Image
+    aspect_ratio = 16/9
+    image_width = 1000
+    image_height = int(image_width/aspect_ratio)
 
-    image_width = 256
-    image_height = 256
+    # Camera
+    viewport_height = 2.0
+    viewport_width = aspect_ratio * viewport_height
+    focal_length = 1.0
+
+    origin = np.array([0, 0, 0])
+    horizontal = np.array([viewport_width, 0, 0])
+    vertical = np.array([0, viewport_height, 0])
+    lower_left_corner = origin - horizontal/2 - \
+        vertical/2 - np.array([0, 0, focal_length])
 
     # Render
-
-    # write to file
     with open("image.ppm", "w+") as image_file:
         header = f"P3\n{image_width} {image_height}\n255\n"
         image_file.write(header)
 
-        for j in tqdm(reversed(range(image_height))):
+        for j in tqdm(reversed(range(image_height)), total=image_height):
             for i in range(image_width):
-                red = i/(image_width-1)
-                green = j/(image_height-1)
-                blue = 0.25
-                color = np.array([red, green, blue])
+                u = float(i / (image_width-1))
+                v = float(j / (image_height-1))
+
+                ray = Ray(origin, direction=lower_left_corner +
+                          u*horizontal + v*vertical - origin)
+
+                color = ray_color(ray)
 
                 write_color(image_file, color)
+
 
 if __name__ == '__main__':
     main()
