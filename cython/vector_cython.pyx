2db9ac76e083af20f955e0f2d8ab14791aab210a
schulze-paul <schulze.paul@protonmail.com>
2022-08-21 23:28:14 +0200

    implementing Colors class in cython

--- cython/vector_cython.pyx
+++ cython/vector_cython.pyx
@@ -53,28 +53,28 @@ cdef class Vector:
         cdef double y = self.y() + other.y()
         cdef double z = self.z() + other.z()
 
-        return Vector(x, y, z)
+        return self.__class__(x, y, z)
 
     def __sub__(self, Vector other):
         cdef double x = self.x() - other.x()
         cdef double y = self.y() - other.y()
         cdef double z = self.z() - other.z()
 
-        return Vector(x, y, z)
+        return self.__class__(x, y, z)
 
     def __mul__(self, double other):
         cdef double x = self.x() * other
         cdef double y = self.y() * other
         cdef double z = self.z() * other
 
-        return Vector(x, y, z)
+        return self.__class__(x, y, z)
 
     def __truediv__(self, double other):
         cdef double x = self.x() / other
         cdef double y = self.y() / other
         cdef double z = self.z() / other
 
-        return Vector(x, y, z)
+        return self.__class__(x, y, z)
 
     def __str__(self):
         return "["+ str(self.x()) +", "+ str(self.y()) +", "+ str(self.z()) +"]"
@@ -84,3 +84,45 @@ cdef class Vector:
             return True
         else:
             return False
+
+
+
+cdef class Color(Vector):
+    """class to store rgb color information"""
+
+    def __init__(self, double r, double g, double b):
+        self.x_val = r
+        self.y_val = g
+        self.z_val = b
+
+    def r(self):
+        return self.__cut(self.x())
+
+    def g(self):
+        return self.__cut(self.y())
+
+    def b(self):
+        return self.__cut(self.z())
+
+    cdef double __cut(self, double val):
+        minimum = 0
+        maximum = 0.999
+        if val < minimum:
+            return minimum
+        if val > maximum:
+            return maximum
+        else:
+            return val
+
+    @staticmethod
+    def from_vector(vec: Vector):
+        return Color(vec.x(), vec.y(), vec.z())
+
+
+cpdef Vector random_in_unit_sphere():
+    while (True):
+        point = Vector.random(-1, 1)
+        if point.length_sq() > 1:
+            pass
+        else:
+            return point
