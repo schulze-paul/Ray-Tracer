defc3154aef32fe15b38a94c99166317eae17ac0
schulze-paul <schulze.paul@protonmail.com>
2022-08-30 13:47:18 +0200

    Creating reflective diffuse material

--- python/hittables.py
+++ python/hittables.py
@@ -4,8 +4,7 @@ import random
 import numpy as np
 
 # local imports
-from hittable import Ray
-from hittable import Vector, Color, dot, unit_vector, random_unit_vector, reflect, refract, random_in_unit_sphere
+from hittable import Ray, Vector, Color, dot, unit_vector, random_unit_vector, reflect, refract, random_in_unit_sphere
 # , Lambertian, Dielectric, Metal
 from hittable import HitRecord, Hittable, Material, Sphere, MovableSphere  # , HittableList
 
@@ -169,6 +168,41 @@ class Dielectric(Material):
         else:
             direction = refract(
                 unit_direction, hit_record.normal, refraction_ratio)
+
+        scattered = Ray(hit_record.hit_point, direction, ray_in.time)
+
+        return True, scattered, attenuation
+
+    def __reflectance(self, cos_theta: float, refraction_ratio: float) -> float:
+        # Schlick's approximation
+        r0 = (1-refraction_ratio) / (1+refraction_ratio)
+        r0 = r0**2
+        return r0 * (1-r0)*(1-cos_theta)**5
+
+
+@dataclass
+class ReflectiveOpaque(Material):
+    albedo: Color
+    index_of_refraction: float
+
+    def scatter(self, ray_in, hit_record):
+        attenuation = Color(1, 1, 1)
+        if hit_record.hit_from_outside == 1:
+            refraction_ratio = 1/self.index_of_refraction
+        else:
+            refraction_ratio = self.index_of_refraction
+
+        unit_direction = unit_vector(ray_in.direction)
+        cos_theta = min(dot(unit_direction*-1, hit_record.normal), 1.0)
+        sin_theta = math.sqrt(1.0-cos_theta**2)
+        cannot_refract = refraction_ratio*sin_theta > 1
+        reflectance = self.__reflectance(cos_theta, refraction_ratio)
+        if cannot_refract or (reflectance > random.random()):
+            direction = reflect(unit_direction, hit_record.normal)
+        else:
+            direction = hit_record.normal + random_unit_vector()
+            if direction.near_zero():
+                direction = hit_record.normal
 
         scattered = Ray(hit_record.hit_point, direction, ray_in.time)
 
