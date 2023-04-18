4ea19283c499a722691b5a8daf6c5f2504276946
schulze.paul <schulze.paul@protonmail.com>
2023-04-18 14:13:58 +0200

    Implemented texture class

--- cpp/texture/solid_color.h
+++ cpp/texture/solid_color.h
@@ -7,8 +7,8 @@ class SolidColor : public Texture
 {
 public:
     SolidColor() {}
-    SolidColor(Vec3 c) : color_value(c) {}
-    SolidColor(double red, double green, double blue) : SolidColor(Vec3(red, green, blue)) {}
+    SolidColor(Color c) : color_value(c) {}
+    SolidColor(double red, double green, double blue) : SolidColor(Color(red, green, blue)) {}
 
     virtual Color value(double u, double v, const Vec3 &p) const override
     {
