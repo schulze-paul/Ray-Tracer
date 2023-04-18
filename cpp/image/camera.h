e6f87abde9a194b49ceff7b45317fa0f3da13394
schulze.paul <schulze.paul@protonmail.com>
2023-04-18 14:08:54 +0200

    Camera: Also save aspect ratio

--- cpp/image/camera.h
+++ cpp/image/camera.h
@@ -20,15 +20,17 @@ public:
         double h = tan(theta / 2);
         this->viewport_height = 2.0 * h;
         this->viewport_width = aspect_ratio * viewport_height;
+        this->aspect_ratio = aspect_ratio;
 
         this->w = unit_vector(look_from - look_at);
         this->u = unit_vector(cross(Vec3(0, 1, 0), w));
         this->v = cross(w, u);
 
-        this->origin = look_from;
+        this->look_from = look_from;
+        this->look_at = look_at;
         this->horizontal = focus_dist * viewport_width * u;
         this->vertical = focus_dist * viewport_height * v;
-        this->lower_left_corner = origin - horizontal / 2 - vertical / 2 - focus_dist * w;
+        this->lower_left_corner = look_from - horizontal / 2 - vertical / 2 - focus_dist * w;
 
         this->lens_radius = aperture / 2;
         this->time0 = time0;
@@ -39,13 +41,14 @@ public:
     {
         Vec3 rd = lens_radius * random_in_unit_disk();
         Vec3 offset = this->u * rd.x() + this->v * rd.y();
-        return Ray(origin + offset,
-                   lower_left_corner + u * horizontal + v * vertical - origin - offset,
+        return Ray(look_from + offset,
+                   lower_left_corner + u * horizontal + v * vertical - look_from - offset,
                    random_double(time0, time1));
     };
 
-private:
-    Vec3 origin;
+public:
+    Vec3 look_from;
+    Vec3 look_at;
     Vec3 lower_left_corner;
     Vec3 horizontal;
     Vec3 vertical;
@@ -54,6 +57,7 @@ private:
     double viewport_width;
     double viewport_height;
     double time0, time1; // shutter open/close times
+    double aspect_ratio;
 };
 
 #endif // CAMERA_H
