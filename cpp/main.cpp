874e262fc19b3c89019db31b4e68ba3dce49f878
schulze.paul <schulze.paul@protonmail.com>
2023-04-20 19:31:52 +0200

    Including changes in main.cpp

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <fstream>
+#include <cmath>
+
 
 #include "ray_tracer.h"
 #include "vec3.h"
@@ -8,18 +11,25 @@
 #include "hittable_list.h"
 #include "camera.h"
 #include "image_data.h"
-#include "scenes.h"
+#include "bvh.cpp"
+#include "rectangle.h"
+#include "progress_bar.h"
+#include "load_scene.h"
 
-Color background_color(const Ray &r)
+
+Color night_sky(const Ray &r)
 {
-    double y_alignment = unit_vector(r.direction).y();
-    double normalized_y_alignment = 0.5f * (y_alignment + 1.0f);
-    Color top_color = Color(0.5f, 0.7f, 1.0f);
-    Color bottom_color = Color(1.0f, 1.0f, 1.0f);
-    return (1.0f - normalized_y_alignment) * bottom_color + normalized_y_alignment * top_color;
+    return Color(0, 0, 0);
 }
 
-Color get_ray_color(const Ray &r, HittableList &world, int depth)
+Color day_sky(const Ray &r)
+{
+    Vec3 unit_direction = unit_vector(r.direction);
+    float t = 0.5f * (unit_direction.y() + 1.0f);
+    return (1.0f - t) * Color(1.0f, 1.0f, 1.0f) + t * Color(0.5f, 0.7f, 1.0f);
+}
+
+Color ray_tracing_shader(const Ray &r, HittableList &world, int depth)
 {
     HitRecord rec;
     // max depth reached
@@ -28,40 +38,77 @@ Color get_ray_color(const Ray &r, HittableList &world, int depth)
         return Color(0, 0, 0);
     }
 
+    // background
+    if (!world.hit(r, 0.001f, 100.0f, rec))
+    {
+        return night_sky(r);
+    }
+
     // hit something
-    if (world.hit(r, 0.0f, 100.0f, rec))
+    Ray scattered;
+    Color attenuation;
+    Color emitted = rec.getMaterial()->emitted(rec.getU(), rec.getV(), rec.getHitPoint());
+    if (!rec.getMaterial()->scatter(r, rec, attenuation, scattered))
+    {
+        return emitted;
+    }
+
+    return emitted + attenuation * ray_tracing_shader(scattered, world, depth - 1);
+}
+
+Color scattering_shader(const Ray &r, HittableList &world, int depth)
+{
+    HitRecord rec = HitRecord();
+    if (world.hit(r, 0.001f, 100.0f, rec))
     {
         Ray scattered;
         Color attenuation;
-        if (rec.getMaterial()->scatter(r, rec, attenuation, scattered))
-        {
-            return attenuation * get_ray_color(scattered, world, depth - 1);
-        }
-        return 0.5f * Color(rec.getNormal().x() + 1.0f, rec.getNormal().y() + 1.0f, rec.getNormal().z() + 1.0f);
-    }
 
-    // background
-    return background_color(r);
+        if (rec.getMaterial()->scatter(r, rec, attenuation, scattered)) 
+        {
+            return scattered.direction*0.5 + Vec3(0.5,0.5, 0.5);
+        }
+    }
+    
+    return night_sky(r);
 }
+
+
+Color normal_shader(const Ray &r, HittableList &world, int depth)
+{
+    HitRecord rec;
+    if (!world.hit(r, 0.001f, 100.0f, rec))
+    {
+        return night_sky(r);
+    }
+    
+    Vec3 normal = rec.isFrontFace(r) ? rec.getNormal() : -rec.getNormal();
+    return normal*0.5 + Vec3(0.5, 0.5, 0.5);
+}
+
 
 int main()
 {
     // world
     Camera camera;
-    HittableList world = get_random_scene_checker(camera);
+    HittableList world = load_scene("scenes/sphere_lighting.yaml", camera);
 
     ImageData image = ImageData(
         400,        // width
-        16.0 / 9.0, // aspect ratio
-        128,        // samples per pixel
-        500);       // max depth
+        camera.aspect_ratio, // aspect ratio
+        1024,        // samples per pixel
+        50);       // max depth
 
+    int total = image.get_height();
+    ProgressBar bar(total);
+    int progress = 0;
     // loop over image
     for (int j = image.get_height() - 1; j >= 0; --j)
     {
-        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+        bar.increment();
         for (int i = 0; i < image.get_width(); ++i)
         {
+            // progress bar
             for (int s = 0; s < image.get_samples_per_pixel(); ++s)
             {
                 // ray
@@ -69,15 +116,12 @@ int main()
                 double v = image.get_v(j);
                 Ray ray = camera.get_ray(u, v);
 
-                Color color = get_ray_color(ray, world, image.get_max_depth());
+                Color color = ray_tracing_shader(ray, world, image.get_max_depth());
                 image.add_color(i, j, color);
             }
         }
     }
 
-    std::cerr << std::endl
-              << "saving image..."
-              << std::endl;
-    image.write_ppm(std::cout);
+    image.write_to_file("sphere_lighting_back_wall.ppm");
     return 0;
 }
