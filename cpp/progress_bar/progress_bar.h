2aadde261ebbec983e406db48a1c28b1f1995f44
schulze.paul <schulze.paul@protonmail.com>
2023-05-02 18:31:34 +0200

    ProgressBar: Also show time/iteration
    Print newline after process is finished

--- cpp/progress_bar/progress_bar.h
+++ cpp/progress_bar/progress_bar.h
@@ -34,8 +34,10 @@ public:
         std::chrono::time_point<std::chrono::system_clock> end = std::chrono::system_clock::now();
         std::chrono::duration<double> elapsed_seconds = end - start;
         double elapsed = elapsed_seconds.count();
+        double timePerIteration = elapsed / progress;
+
         double eta = elapsed / progress * (total - progress);
-        std::cerr << "[";
+        std::cerr << " |";
         int pos = barWidth * progress / total;
         for (int i = 0; i < barWidth; ++i)
         {
@@ -48,11 +50,16 @@ public:
             else
                 std::cerr << " ";
         }
-        std::cerr << "] " << int(progress * 100.0 / total) << " %\t";
-        std::cerr << "Progress: " << progress << " / " << total << " \t";
-        std::cerr << "Elapsed: " << seconds_to_time(elapsed) << " \t";
-        std::cerr << "ETA: " << seconds_to_time(eta) << " \r";
+        std::cerr << "| " << progress << " / " << total; 
+        std::cerr << " [" << seconds_to_time(elapsed) << " -> ";
+        std::cerr << seconds_to_time(eta) << "] ";
+        std::cerr << (double)((int)(timePerIteration*100))/100.0 << "s/it \t\t\r";
         std::cerr.flush();
+
+        if (progress == total)
+        {
+            std::cerr << std::endl;
+        }
     }
     void increment()
     {
