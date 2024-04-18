810693e30f8d69f01055dd19930589e529c259e4
paul.schulze <paul.schulze@tum.de>
2024-04-18 17:51:07 +0200

    Hotfix: avoid double hit

--- ray-tracer/src/material.rs
+++ ray-tracer/src/material.rs
@@ -37,14 +37,16 @@ impl MetalStruct {
         MetalStruct{albedo, fuzz}
     }
     fn scatter(&self, ray: &Ray, hit_record: &HitRecord) -> Ray {
+        println!("{:?}, {:?}, {:?}, {:?}", hit_record.hit_point,ray.direction, hit_record.normal, reflect(ray.direction, hit_record.normal));
         return Ray::new(
-            hit_record.hit_point,
+            // move away from hit point to avoid double hit
+            hit_record.hit_point + hit_record.normal/1000.0, 
             reflect(ray.direction, hit_record.normal))
     }
 }
 
-fn reflect(v_in: Vec3, normal: Vec3) -> Vec3 {
-    v_in - 2.0*dot(v_in, normal) * normal
+fn reflect(v: Vec3, n: Vec3) -> Vec3 {
+    return v - 2.0 * dot(v, n) * n;
 }
 
 impl DielectricStruct {
@@ -57,7 +59,6 @@ impl DielectricStruct {
         } else {
             self.refractive_index
         };
-
         let unit_normal = if hit_record.is_front_face(ray) {
             hit_record.normal
         } else {
@@ -67,27 +68,31 @@ impl DielectricStruct {
         let sin_theta = (1.0 - cos_theta * cos_theta).sqrt();
 
         let cannot_refract = refraction_ratio * sin_theta > 1.0;
-        
-        let scatter_direction: Vec3;
+        let mut scatter_direction: Vec3;
         if cannot_refract || reflectance(cos_theta, refraction_ratio) > random_float() {
             scatter_direction = reflect(ray.direction, unit_normal);
         } else {
             scatter_direction = refract(ray.direction, unit_normal, refraction_ratio);
         }
-        return Ray::new(hit_record.hit_point, scatter_direction);
+        scatter_direction = refract(ray.direction, unit_normal, refraction_ratio);
+        return Ray::new(
+            hit_record.hit_point + unit_normal/1000.0, // avoid double hit 
+            scatter_direction);
         
     }
 }
 fn refract(v_in: Vec3, normal: Vec3, refraction_ratio: f64) -> Vec3 {
-    let cos_theta = dot(-v_in, normal).min(1.0);
-    let out_perpendiculat = refraction_ratio * (v_in + cos_theta*normal);
-    let out_parallel = -(1.0 - out_perpendiculat.length_squared()).abs().sqrt() * normal;
-    return out_perpendiculat + out_parallel;
+    let cos_theta = f64::min(dot(-v_in, normal), 1.0);
+    let out_perpendicular = refraction_ratio * (v_in + cos_theta*normal);
+    let out_parallel = -(1.0 - out_perpendicular.length_squared()).abs().sqrt() * normal;
+    return out_perpendicular + out_parallel;
 
 }
 fn reflectance(cosine: f64, ref_idx: f64) -> f64 {
     // Use Schlick's approximation for reflectance.
-    let r0 = ((1.0 - ref_idx) / (1.0 + ref_idx)).powi(2);
+    let r0 = (
+            (1.0 - ref_idx) / (1.0 + ref_idx)
+        ).powi(2);
     return r0 + (1.0 - r0) * (1.0 - cosine).powi(5);
 }
 
